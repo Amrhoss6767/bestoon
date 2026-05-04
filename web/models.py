4adf4cbe8b6e47747a177a30a9312c008e991bbe
abbas <bashiri1367amirhossein@gmail.com>
2026-05-04 23:17:10 +0330

    income in models and some beautifications

--- web/models.py
+++ web/models.py
@@ -9,5 +9,34 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete = models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date , self.amount)
+    
+        
+        
+         
+    
 
 
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date , self.amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
